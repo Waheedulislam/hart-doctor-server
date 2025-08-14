c34ff9a133bfa5df9c6ef82272029f836aed1edc
Waheedul Islam <waheeduli81@gmail.com>
2025-08-15 01:03:27 +0600

    secret-pass

--- src/app/modules/courses/courses.route.ts
+++ src/app/modules/courses/courses.route.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { CourseController } from "./courses.controller";
+import { checkSecurePassword } from "../../middleware/checkSecurePassword";
 
 const router = Router();
 
 router.get("/", CourseController.getAllCourse);
 router.get("/:courseId", CourseController.getSingleCourse);
-router.post("/create-course", CourseController.CreateCourse);
+router.post(
+  "/create-course",
+  checkSecurePassword,
+  CourseController.CreateCourse
+);
 
-router.patch("/:courseId", CourseController.updateCourse);
-router.delete("/:courseId", CourseController.deleteCourse);
+router.patch("/:courseId", checkSecurePassword, CourseController.updateCourse);
+router.delete("/:courseId", checkSecurePassword, CourseController.deleteCourse);
 export const CourseRoutes = router;
