fcbbf35cadafd027f6a8a2700f9de56073838665
Waheedul Islam <waheeduli81@gmail.com>
2025-08-16 03:55:22 +0600

    re-build

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -7,8 +7,5 @@ export default {
   NODE_ENV: process.env.NODE_ENV,
   port: process.env.PORT,
   db_url: process.env.DB_URL,
-  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
-  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
   secure_password: process.env.SECURE_PASSWORD,
 };
